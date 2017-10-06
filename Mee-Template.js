0f582822e39c89806c9c9375d16ae3f8124f2ff9
waheedsd <syedwaheed9292@gmail.com>
2017-10-06 10:33:45 +0530

    cosmicvent

---
+++ Mee-Template.js
@@ -0,0 +1,10 @@
+function changeText(id){
+	id.innerHTML="cooming soon!";
+}
+function changeIn(id){
+	id.style.backgroundColor="grey";
+}
+function changeOut(id)
+{
+	id.style.backgroundColor="transparent";
+}
